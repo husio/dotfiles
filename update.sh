ae4a53ba8fd611f108b1cf17898bf02c74011da4
Piotr Husiatyński <phusiatynski@gmail.com>
2017-12-08 18:58:55 +0100

    up

--- update.sh
+++ update.sh
@@ -1,20 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
 
 REPO=`pwd`
 
-cp $HOME/.Xdefaults $REPO/Xdefaults
-cp $HOME/.xinitrc $REPO/xinitrc
-cp $HOME/.xsession $REPO/xsession
-cp $HOME/.tmux.conf $REPO/tmux.conf
-cp $HOME/.hgrc $REPO/hgrc
-cp $HOME/.gitconfig $REPO/gitconfig
 
-cp $HOME/.mplayer $REPO/mplayer -R
-cp $HOME/.ncmpc   $REPO/ncmpc -R
+backup() {
+	mkdir -p `dirname $2`
+	cp -R "$1" "$2"
+}
 
-mkdir $REPO/config 2> /dev/null
 
-mkdir $REPO/config/awesome 2> /dev/null
-cp $HOME/.config/awesome/rc.lua $REPO/config/awesome/rc.lua
-cp $HOME/.config/awesome/theme.lua $REPO/config/awesome/theme.lua
+backup $HOME/.bin/radio $REPO/bin/radio
+backup $HOME/.bin/setup_keyboard $REPO/bin/setup_keyboard
+backup $HOME/.bin/setup_mouse $REPO/bin/setup_mouse
+backup $HOME/.bin/setup_screen $REPO/bin/setup_screen
+backup $HOME/.bin/subdl $REPO/bin/subdl
+backup $HOME/.bin/up $REPO/bin/up
+backup $HOME/.bin/youtube-download-mp3 $REPO/bin/youtube-download-mp3
+
+backup $HOME/.Xdefaults $REPO/Xdefaults
+backup $HOME/.config/i3   $REPO/i3 -R
+backup $HOME/.gitconfig $REPO/gitconfig
+backup $HOME/.i3status.conf $REPO/i3status.conf
+backup $HOME/.pythonrc $REPO/pythonrc
+backup $HOME/.tmux.conf $REPO/tmux.conf
+backup $HOME/.tmux.conf $REPO/tmux.conf
+backup $HOME/.wallpaper.png $REPO/wallpaper.png
+backup $HOME/.xinitrc $REPO/xinitrc
+backup $HOME/.xsession $REPO/xsession
+backup $HOME/.zshrc.local $REPO/zshrc.local
