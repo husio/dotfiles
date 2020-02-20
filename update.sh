bcd7feae3601c5ed036a501e8807ba6d50de478a
Piotr Husiatyński <phusiatynski@gmail.com>
2020-02-20 20:51:34 +0100

    update

--- update.sh
+++ update.sh
@@ -15,12 +15,12 @@ backup $HOME/.bin/radio $REPO/bin/radio
 backup $HOME/.bin/setup_keyboard $REPO/bin/setup_keyboard
 backup $HOME/.bin/setup_mouse $REPO/bin/setup_mouse
 backup $HOME/.bin/setup_screen $REPO/bin/setup_screen
-backup $HOME/.bin/subdl $REPO/bin/subdl
 backup $HOME/.bin/up $REPO/bin/up
 backup $HOME/.bin/youtube-download-mp3 $REPO/bin/youtube-download-mp3
+backup $HOME/.bin/playtwitch $REPO/bin/playtwitch
 
 backup $HOME/.Xdefaults $REPO/Xdefaults
-backup $HOME/.config/i3   $REPO/i3 -R
+backup $HOME/.config/i3/config $REPO/i3/
 backup $HOME/.gitconfig $REPO/gitconfig
 backup $HOME/.i3status.conf $REPO/i3status.conf
 backup $HOME/.pythonrc $REPO/pythonrc
@@ -30,3 +30,4 @@ backup $HOME/.wallpaper.png $REPO/wallpaper.png
 backup $HOME/.xinitrc $REPO/xinitrc
 backup $HOME/.xsession $REPO/xsession
 backup $HOME/.zshrc.local $REPO/zshrc.local
+backup $HOME/.gtkrc-2.0 $REPO/gtkrc-2.0
